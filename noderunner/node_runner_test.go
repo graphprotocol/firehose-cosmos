8ca3f04f4e6be9a95a53cd5965d3dc5ebe0f9ac5
Dan Sosedoff <dan.sosedoff@gmail.com>
2022-03-01 17:58:47 -0600

    Uncomment tests

--- noderunner/node_runner_test.go
+++ noderunner/node_runner_test.go
@@ -3,36 +3,37 @@ package noderunner
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestNodeRunner(t *testing.T) {
-	// t.Run("basic", func(t *testing.T) {
-	// 	runner := New("true", []string{}, true)
+	t.Run("basic", func(t *testing.T) {
+		runner := New("true", []string{}, true)
 
-	// 	err := runner.Start(context.Background())
-	// 	assert.Nil(t, err)
-	// })
+		err := runner.Start(context.Background())
+		assert.Nil(t, err)
+	})
 
-	// t.Run("invalid command", func(t *testing.T) {
-	// 	runner := New("foo", []string{}, true)
+	t.Run("invalid command", func(t *testing.T) {
+		runner := New("foo", []string{}, true)
 
-	// 	err := runner.Start(context.Background())
-	// 	assert.Error(t, err)
-	// 	assert.Equal(t, `exec: "foo": executable file not found in $PATH`, err.Error())
-	// })
+		err := runner.Start(context.Background())
+		assert.Error(t, err)
+		assert.Equal(t, `exec: "foo": executable file not found in $PATH`, err.Error())
+	})
 
-	// t.Run("with cancel", func(t *testing.T) {
-	// 	runner := New("sleep", []string{"10"}, true)
+	t.Run("with cancel", func(t *testing.T) {
+		runner := New("sleep", []string{"10"}, true)
 
-	// 	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*500)
-	// 	defer cancel()
+		ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*500)
+		defer cancel()
 
-	// 	err := runner.Start(ctx)
-	// 	assert.Error(t, err)
-	// 	assert.Equal(t, "signal: interrupt", err.Error())
-	// })
+		err := runner.Start(ctx)
+		assert.Error(t, err)
+		assert.Equal(t, "signal: interrupt", err.Error())
+	})
 
 	t.Run("line reader", func(t *testing.T) {
 		lines := []string{}
