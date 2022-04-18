f5119feabe0d66d92b5069b8e6103e92e6448f51
Dan Sosedoff <dan.sosedoff@gmail.com>
2022-04-18 10:49:02 -0700

    Misc changes to allow running the osmosisd binary

--- devel/osmosis/bootstrap.sh
+++ devel/osmosis/bootstrap.sh
@@ -14,6 +14,7 @@ OSMOSIS_VERSION=${OSMOSIS_VERSION:-"v1.0.1"}
 OSMOSIS_GENESIS="https://github.com/osmosis-labs/networks/raw/main/osmosis-1/genesis.json"
 OSMOSIS_GENESIS_HEIGHT=${OSMOSIS_GENESIS_HEIGHT:-"1"}
 OSMOSIS_ADDRESS_BOOK="https://quicksync.io/addrbook.osmosis.json"
+OSMOSIS_PATH="osmosisd"
 
 case $OS_PLATFORM-$OS_ARCH in
   Darwin-x86_64) OSMOSIS_PLATFORM="darwin_amd64" ;;
@@ -38,25 +39,25 @@ pushd tmp
 
 echo "Your platform is $OS_PLATFORM/$OS_ARCH"
 
-if [ ! -f "osmosisd" ]; then
-  echo "Downloading osmosisd $OSMOSIS_VERSION binary"
-  #wget --quiet -O ./gaiad "https://github.com/figment-networks/gaia-dm/releases/download/$GAIA_VERSION/gaiad_${GAIA_VERSION}_deepmind_$GAIA_PLATFORM"
+if [ ! -f "$OSMOSIS_PATH" ]; then
+  echo "NOTE: Downloading instrumented binaries is not implemented yet"
+  # echo "Downloading osmosisd $OSMOSIS_VERSION binary"
+  #wget --quiet -O ./gaiad "https://github.com/figment-networks/osmisis-dm/releases/download/$OSMOSIS_VERSION/gaiad_${OSMOSIS_VERSION}_deepmind_$OSMOSIS_PLATFORM"
   #chmod +x ./gaiad
-  cp $GOPATH/src/github.com/osmosis-labs/osmosis/build/osmosisd .
+fi
+
+if [[ -z $(which osmosisd || true) ]]; then
+  echo "Please make sure you have installed local osmosisd $OSMOSIS_VERSION binary"
+  exit 1
 fi
 
 if [ ! -d "osmosis_home" ]; then
   echo "Configuring chain home directory"
-  ./osmosisd --home=osmosis_home init $(hostname)
+  $OSMOSIS_PATH --home=osmosis_home init $(hostname)
   rm -f \
     osmosis_home/config/genesis.json \
     osmosis_home/config/addrbook.json
 fi
-
-# if [ ! -f "osmosis_home/config/config.toml" ]; then
-#   echo "Fetching gaia config"
-#   # TODO
-# fi
 
 if [ ! -f "osmosis_home/config/genesis.json" ]; then
   echo "Downloading osmosis genesis file"
@@ -83,13 +84,13 @@ if [ ! -f "firehose.yml" ]; then
 start:
   args:
     - ingestor
-    - merger
-    - firehose
-    - relayer
+    #- merger
+    #- firehose
+    #- relayer
   flags:
     common-first-streamable-block: $OSMOSIS_GENESIS_HEIGHT
     ingestor-mode: node
-    ingestor-node-path: ./osmosisd
+    ingestor-node-path: $(which osmosisd)
     ingestor-node-args: start --x-crisis-skip-assert-invariants --home=./osmosis_home
 END
 fi
