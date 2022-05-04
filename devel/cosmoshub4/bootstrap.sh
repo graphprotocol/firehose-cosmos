3e665573302d523c1c4cfa844614d03e9887e4cd
Dan Sosedoff <dan.sosedoff@gmail.com>
2022-05-03 19:24:40 -0500

    Add support for cosmoshub4 testnet

--- devel/cosmoshub4/bootstrap.sh
+++ devel/cosmoshub4/bootstrap.sh
@@ -4,15 +4,29 @@ set -o errexit
 set -o pipefail
 
 CLEANUP=${CLEANUP:-"0"}
-
+NETWORK=${NETWORK:-"mainnet"}
 OS_PLATFORM=$(uname -s)
 OS_ARCH=$(uname -m)
-
 GAIA_PLATFORM=${GAIA_PLATFORM:-"linux_amd64"}
-GAIA_VERSION=${GAIA_VERSION:-"v4.2.1"}
-GAIA_GENESIS="https://github.com/cosmos/mainnet/raw/master/genesis.cosmoshub-4.json.gz"
-GAIA_GENESIS_HEIGHT=${GAIA_GENESIS_HEIGHT:-"5200791"}
-GAIA_ADDRESS_BOOK="https://quicksync.io/addrbook.cosmos.json"
+
+case $NETWORK in
+  mainnet)
+    echo "Using MAINNET"
+    GAIA_VERSION=${GAIA_VERSION:-"v4.2.1"}
+    GAIA_GENESIS="https://github.com/cosmos/mainnet/raw/master/genesis.cosmoshub-4.json.gz"
+    GAIA_GENESIS_HEIGHT=${GAIA_GENESIS_HEIGHT:-"5200791"}
+    GAIA_ADDRESS_BOOK="https://quicksync.io/addrbook.cosmos.json"
+  ;;
+  testnet)
+    echo "Using TESTNET"
+    GAIA_VERSION=${GAIA_VERSION:-"v6.0.0"}
+    GAIA_GENESIS="https://github.com/cosmos/testnets/raw/master/v7-theta/public-testnet/genesis.json.gz"
+    GAIA_GENESIS_HEIGHT=${GAIA_GENESIS_HEIGHT:-"9034670"}
+  ;;
+  *)
+    echo "Invalid network: $NETWORK"; exit 1;
+  ;;
+esac
 
 case $OS_PLATFORM-$OS_ARCH in
   Darwin-x86_64) GAIA_PLATFORM="darwin_amd64" ;;
@@ -28,7 +42,7 @@ fi
 
 if [[ $CLEANUP -eq "1" ]]; then
   echo "Deleting all local data"
-  rm -rf ./tmp/
+  rm -rf ./tmp/ > /dev/null
 fi
 
 echo "Setting up working directory"
@@ -39,33 +53,36 @@ echo "Your platform is $OS_PLATFORM/$OS_ARCH"
 
 if [ ! -f "gaiad" ]; then
   echo "Downloading gaiad $GAIA_VERSION binary"
-  wget -O ./gaiad "https://github.com/figment-networks/gaia-dm/releases/download/$GAIA_VERSION/gaiad_${GAIA_VERSION}_firehose_$GAIA_PLATFORM"
+  wget --quiet -O ./gaiad "https://github.com/figment-networks/gaia-dm/releases/download/$GAIA_VERSION/gaiad_${GAIA_VERSION}_firehose_$GAIA_PLATFORM"
   chmod +x ./gaiad
 fi
 
 if [ ! -d "gaia_home" ]; then
-  echo "Configuring gaia home directory"
+  echo "Configuring home directory"
   ./gaiad --home=gaia_home init $(hostname) 2> /dev/null
   rm -f \
     gaia_home/config/genesis.json \
     gaia_home/config/addrbook.json
 fi
 
-if [ ! -f "gaia_home/config/config.toml" ]; then
-  echo "Fetching gaia config"
-  # TODO
-fi
-
 if [ ! -f "gaia_home/config/genesis.json" ]; then
-  echo "Downloading gaia genesis file"
+  echo "Downloading genesis file"
   wget --quiet -O gaia_home/config/genesis.json.gz $GAIA_GENESIS
   gunzip gaia_home/config/genesis.json.gz
 fi
 
-if [ ! -f "gaia_home/config/addrbook.json" ]; then
-  echo "Downloading address book"
-  wget --quiet -O gaia_home/config/addrbook.json $GAIA_ADDRESS_BOOK
-fi
+case $NETWORK in
+  mainnet) # Using addrbook will ensure fast block sync time
+    if [ ! -f "gaia_home/config/addrbook.json" ]; then
+      echo "Downloading address book"
+      wget --quiet -O gaia_home/config/addrbook.json $GAIA_ADDRESS_BOOK
+    fi
+  ;;
+  testnet) # There's no address book for the testnet, use seeds instead
+    echo "Configuring p2p seeds"
+    sed -i -e 's/seeds = ""/seeds = "639d50339d7045436c756a042906b9a69970913f@seed-01.theta-testnet.polypore.xyz:26656,3e506472683ceb7ed75c1578d092c79785c27857@seed-02.theta-testnet.polypore.xyz:26656"/g' gaia_home/config/config.toml
+  ;;
+esac
 
 cat << END >> gaia_home/config/config.toml
 
@@ -84,9 +101,9 @@ start:
     - ingestor
     - merger
     - firehose
-    - relayer
   flags:
     common-first-streamable-block: $GAIA_GENESIS_HEIGHT
+    common-blockstream-addr: localhost:9000
     ingestor-mode: node
     ingestor-node-path: ./gaiad
     ingestor-node-args: start --x-crisis-skip-assert-invariants --home=./gaia_home
