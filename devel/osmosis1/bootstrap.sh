c1245d0d5948c0d4a8bdb44f5c34cd8a3d9dae23
Mark Preston <mark@lavabyrd.space>
2022-07-14 20:19:01 +0100

    fix osmosis bootstrap script

--- devel/osmosis1/bootstrap.sh
+++ devel/osmosis1/bootstrap.sh
@@ -88,7 +88,7 @@ start:
     - firehose
   flags:
     common-first-streamable-block: $OSMOSIS_GENESIS_HEIGHT
-    common-blockstream-addr: localhost:9000
+    common-blockstream-addr:
     ingestor-mode: node
     ingestor-node-path: $(which osmosisd)
     ingestor-node-args: start --x-crisis-skip-assert-invariants --home=./osmosis_home
